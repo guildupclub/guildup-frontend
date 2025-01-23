f480187063ba080e4a652d210cd8471000466f04
Abro0058T <abhiseknaulae@gmail.com>
2025-01-23 12:04:51 +0530

    create explore section need to replace trending posts with trending tags

---
+++ src/components/explore/MemoizedCommunityCard.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Card } from '../ui/card';
+import Image from 'next/image';
+
+function MemoizedCommunityCard({ community, onClick }: { community: any; onClick: (id: string) => void }) {
+    return (
+        <Card
+          onClick={() => onClick(community._id)}
+          className="mb-4 break-inside-avoid border-none rounded-lg bg-[#19191A] w-[292px] overflow-hidden shadow-md"
+        >
+          <div className="p-3 pb-0 flex justify-center align-center">
+            <div className="relative aspect-video h-[100px] w-[100%] rounded-lg overflow-hidden">
+              <Image
+                src={community.image || "/defaultCommunityIcon.png"}
+                alt={community.name}
+                fill
+                className="object-cover"
+              />
+            </div>
+          </div>
+          <div className="p-4">
+            <div className="flex items-center space-x-2 mb-2">
+              <Image
+                src={community.icon || "/defaultCommunityIcon.png"}
+                alt={community.name}
+                width={24}
+                height={24}
+                className="rounded-full"
+              />
+              <h3 className="font-semibold text-white text-[18px]">{community.name}</h3>
+            </div>
+            <p className="text-sm text-gray-400 mb-4 lg:h-[9vh] md:h-[4vh] sm:h-[4vh] overflow-hidden text-ellipsis line-clamp-3">
+              {community.description}
+            </p>
+            <div className="flex items-center justify-between">
+              <span className="text-xs text-gray-500">{community.num_member} members</span>
+            </div>
+          </div>
+        </Card>
+      );
+}
+
+export default React.memo(MemoizedCommunityCard)
---
+++ src/components/explore/TrendingSection.tsx
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+function TrendingSection() {
+
+    const [trendingPost, setTrensingPost] = useState([])
+
+    useEffect(()=>{
+        
+        const fetchTrendingPost=async ()=>{
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/v1/post/trending`)
+            setTrensingPost(response.data.data)
+        }
+        fetchTrendingPost()
+    },[])
+  return (
+    <div className='bg-[#2B2B2A] p-2 text-white border-none rounded-lg h-[60ch] overflow-scroll [&::-webkit-scrollbar]:hidden'>
+        {
+            trendingPost.length>0 &&(
+                <div>{
+                    trendingPost.map((post:any)=>(
+                        <div className='p-2 border-b-[1px] border-gray-500'>
+                        <div>{post.title}</div>
+                        <p className='text-sm '>{post.up_votes}</p>
+                        </div>
+                    ))}</div>
+            )
+        }
+    </div>
+  )
+}
+
+export default TrendingSection
