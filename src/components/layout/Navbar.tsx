8dfe12d8ff936f4d034bed97c6b22b3bdbfffb47
swapnil-sharma7222 <2021uee1371@mnit.ac.in>
2025-03-09 19:34:47 +0530

    Spelling fixed

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -16,7 +16,7 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Image from "next/image";
 import guildup_logo from "../../../public/svg/GuildUp_Logo_Light.svg";
-import Giuldup_logo_mobile from './../../../public/GuildUp_logo_mobile.svg'
+import Guildup_logo_mobile from './../../../public/GuildUp_logo_mobile.svg'
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
@@ -92,7 +92,7 @@ export function Navbar(props: React.HTMLAttributes<HTMLElement>) {
         <div className="container flex h-14 items-center px-5">
           <Link href="/" className="flex items-center space-x-2 mr-6">
             <Image
-              src={Giuldup_logo_mobile}
+              src={Guildup_logo_mobile}
               alt="GuildUp logo"
               className="h-8 w-auto md:hidden"
             />
