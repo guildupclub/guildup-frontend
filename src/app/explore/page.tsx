adb93f04e85a1f703c77e445e09c904e68554b50
swapnil-sharma7222 <2021uee1371@mnit.ac.in>
2025-03-12 20:23:26 +0530

    scroll issue fixed in explore and community page in mobile view

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -50,7 +50,7 @@ function Page() {
   }
 
   return (
-    <div className="bg-background">
+    <div className="bg-background pb-16">
         <CategoryBar
           categorys={category}
           selectCategory={setSelectedCategory}
