5a0550a00bdb75ece1f520401f013d927aadbfd2
toyesh30 <68783812+toyesh30@users.noreply.github.com>
2025-03-16 23:16:11 +0530

    Update constants.ts
    Signed-off-by: toyesh30 <68783812+toyesh30@users.noreply.github.com>

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 
-// export const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL || "https://guildup-be-569548341732.asia-south1.run.app";
-export const API_BASE_URL = "https://guildup-be-569548341732.asia-south1.run.app";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL || "https://guildup-be-569548341732.asia-south1.run.app";
+// export const API_BASE_URL = "https://guildup-be-569548341732.asia-south1.run.app";
 export const API_ENDPOINTS = {
   getPosts: `${API_BASE_URL}/v1/post/getPosts`,
   getUserCommunity: `${API_BASE_URL}/v1/community/user/follow`,
