7c8910dc9a20cf3e74a1b828cd6367775ed59249
Ayush Bhardwaj <68396415+startlingmind@users.noreply.github.com>
2025-03-19 12:57:13 +0530

    feat: added three steps payment part (#50) (#52)
    * feat: added three steps payment part
    
    * feat: enhance offering dialog and profile card with bank details and calendar connection state management
    
    * feat: add testimonials component with carousel and styling enhancements
    
    * refactor: comment out testimonials section in ProfileCard component
    
    
    
    Earlier PR was against vercel-prod ----- merging same to dev
    
    Co-authored-by: Adarsh Singh <singhadarsh9540@gmail.com>

---
+++ src/components/testimonial/data.ts
@@ -0,0 +1,39 @@
+export const testimonials = [
+  {
+    text: "I’ve always wanted to help the less fortunate and in order to make this impact I wanted to start my own NGO but didn’t know where to start. This is where Project Clay came in. They connected me to mentors that helped me build my website, register my NGO and truly start to make an impact in the community around me. Today, thanks to the mentors at Project Clay I can finally live out my dream of helping those around me!.",
+    name: "Anjani",
+    image:
+      "https://framerusercontent.com/images/5qRrkKzSjcCSovyIzailFQbE4.jpeg?scale-down-to=512",
+  },
+  {
+    text: "I was looking for personalised guidance with my college applications and passion projects, and Project Clay really impressed me.The mentors are incredibly supportive and ruly understand my needs.They matched me with a mentor at my dream university who offered personalised advice that made a huge impact in my projects and college applications. I highly recommend Project Clay to any student seeking college counselling. Thank you Project Clay Team!",
+    name: "Sai Charan",
+    image:
+      "https://framerusercontent.com/images/EfkBmrwDJ09UqQmtG7DTie4Y.jpeg?scale-down-to=512",
+  },
+  {
+    text: "I was overwhelmed with the college application process and unsure of where to start and this is when I stumbled upon Project Clay! They connected me to a mentor who gave me personalised support, helped me identify my strengths and craft a compelling narrative for my applications. Thanks to Project Clay I applied to all my dream universities with personalised guidance from students at that university and put my best foot forward!",
+    name: "Nineesha",
+    image:
+      "https://framerusercontent.com/images/WaNA7GRfj4hvzdOASOMnx1QbJP4.jpeg?scale-down-to=512",
+  },
+
+  {
+    text: "I’ve always wanted to help the less fortunate and in order to make this impact I wanted to start my own NGO but didn’t know where to start. This is where Project Clay came in. They connected me to mentors that helped me build my website, register my NGO and truly start to make an impact in the community around me. Today, thanks to the mentors at Project Clay I can finally live out my dream of helping those around me!.",
+    name: "Anjani",
+    image:
+      "https://framerusercontent.com/images/5qRrkKzSjcCSovyIzailFQbE4.jpeg?scale-down-to=512",
+  },
+  {
+    text: "I was looking for personalised guidance with my college applications and passion projects, and Project Clay really impressed me.The mentors are incredibly supportive and ruly understand my needs.They matched me with a mentor at my dream university who offered personalised advice that made a huge impact in my projects and college applications. I highly recommend Project Clay to any student seeking college counselling. Thank you Project Clay Team!",
+    name: "Sai Charan",
+    image:
+      "https://framerusercontent.com/images/EfkBmrwDJ09UqQmtG7DTie4Y.jpeg?scale-down-to=512",
+  },
+  {
+    text: "I was overwhelmed with the college application process and unsure of where to start and this is when I stumbled upon Project Clay! They connected me to a mentor who gave me personalised support, helped me identify my strengths and craft a compelling narrative for my applications. Thanks to Project Clay I applied to all my dream universities with personalised guidance from students at that university and put my best foot forward!",
+    name: "Nineesha",
+    image:
+      "https://framerusercontent.com/images/WaNA7GRfj4hvzdOASOMnx1QbJP4.jpeg?scale-down-to=512",
+  },
+];
