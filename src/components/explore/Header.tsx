81e08afb76793bff3b272de86ffd7263d5979f96
Adarsh Singh <singhadarsh9540@gmail.com>
2025-03-22 23:30:20 +0530

    fix: fixed node build error

--- src/components/explore/Header.tsx
+++ src/components/explore/Header.tsx
@@ -5,7 +5,8 @@ export default function Header() {
     <div className="flex flex-col items-center justify-center text-center pt-[29px]">
       <h1 className="text-2xl font-bold">Discover Experts</h1>
       <p className="mt-2 text-[15px] leading-tight text-muted-foreground">
-        Why figure it out alone when you can learn from someone who's already
+        Why figure it out alone when you can learn from someone who&rsquo;s
+        already
         <br /> nailed it? Tech, Careers, Finance, and beyond!
       </p>
     </div>
