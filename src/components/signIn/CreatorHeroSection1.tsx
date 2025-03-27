82a7ae021f2e8b87b747990c1fabd99f9f2fe462
Adarsh Singh <singhadarsh9540@gmail.com>
2025-03-27 14:23:10 +0530

    Re redndering problem in profile page (#73)
    * Login page mobile view makeshift fix
    
    * fix: fixed signup issue and redsign signup page
    
    * feat: add loader component to ProfileCard and improve conditional rendering
    
    * fixed: resolve commments
    
    ---------
    
    Co-authored-by: swapnil-sharma7222 <2021uee1371@mnit.ac.in>

--- src/components/signIn/CreatorHeroSection1.tsx
+++ src/components/signIn/CreatorHeroSection1.tsx
@@ -1,41 +1,41 @@
 import Image from "next/image";
-import guildup_logo from './../../../public/svg/GuildUp_Logo_Light.svg';
+import guildup_logo from "./../../../public/svg/GuildUp_Logo_Light.svg";
 import Login_laptop from "./../../../public/Login_laptop.png";
 
 export const CreatorHeroSection1: React.FC = () => {
-    return (
-      <div className="w-1/2 bg-white flex flex-col items-center justify-start p-8 gap-16 border border-background">
+  return (
+    <div className="w-full bg-white flex flex-col items-center justify-start p-8 gap-16 border border-background">
+      <div className="flex justify-center w-full">
+        <Image
+          src={guildup_logo || "/placeholder.svg"}
+          alt="GuildUp"
+          className="h-16 w-auto md:block"
+        />
+      </div>
+
+      <div className="flex flex-col items-center gap-8 w-full max-w-3xl">
+        <div className="flex flex-col gap-3 text-center">
+          <h1 className="text-4xl font-bold">
+            Be found. GuildUp makes it easy.
+          </h1>
+          <p className="text-xl font-normal">
+            Sign up on GuildUp and grow your Guild with thousands of users
+            looking for top coaches, consultants, and professionals.{" "}
+            <span className="font-semibold text-[#334BFF]">
+              Build your custom community and start earning in under 30 minutes!
+            </span>
+          </p>
+        </div>
+
         <div className="flex justify-center w-full">
           <Image
-            src={guildup_logo || "/placeholder.svg"}
+            src={Login_laptop || "/placeholder.svg"}
             alt="GuildUp"
-            className="h-16 w-auto md:block"
+            className="h-[360px] w-auto md:block"
+            priority
           />
         </div>
-
-        <div className="flex flex-col items-center gap-8 w-full max-w-3xl">
-          <div className="flex flex-col gap-3 text-center w-3/4">
-            <h1 className="text-4xl font-bold">
-              Be found. GuildUp makes it easy.
-            </h1>
-            <p className="text-2xl font-normal">
-              Sign up on GuildUp and grow your Guild with thousands of users looking 
-              for top coaches, consultants, and professionals.{" "}
-              <span className="font-semibold text-[#334BFF]">
-                Build your custom community and start earning in under 30 minutes!
-              </span>
-            </p>
-          </div>
-
-          <div className="flex justify-center w-full">
-            <Image
-              src={Login_laptop || "/placeholder.svg"}
-              alt="GuildUp"
-              className="h-[360px] w-auto md:block"
-              priority
-            />
-          </div>
-        </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
--- src/components/signIn/RightSection.tsx
+++ src/components/signIn/RightSection.tsx
@@ -14,7 +14,7 @@ export const RightSection: React.FC = () => {
   };
 
   return (
-    <div className="w-1/2 flex items-center justify-center bg-gradient-to-br from-[#9900FA] to-[#334BFF] p-8">
+    <div className="w-full h-screen flex items-center justify-center bg-gradient-to-br from-[#9900FA] to-[#334BFF]  lg:p-8">
       {session && isFormOpen ? (
         <CreatorForm onSuccess={handleSuccess} /> // when logged in show creator form
       ) : (
