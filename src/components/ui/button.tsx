8c3bf31746a2dd1fcfadc640eefe2ac58770229c
Adarsh Singh <singhadarsh9540@gmail.com>
2025-05-13 18:01:16 +0530

    fix: improve border button of browse  category

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-background hover:text-muted",
+          "border border-input bg-background/30 shadow-sm hover:bg-background hover:text-muted",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-background hover:text-accent-foreground",
