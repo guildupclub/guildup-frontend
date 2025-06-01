8f20ed7df249f301ab7c79edb316fb099f4af23c
Adarsh Singh <singhadarsh9540@gmail.com>
2025-06-01 17:03:19 +0530

    feat: implement global post feature and also add channel delete route

--- src/components/homePageLayout/Feed.tsx
+++ src/components/homePageLayout/Feed.tsx
@@ -8,17 +8,13 @@ import Loader from "../Loader";
 import { PostCarde } from "./PostCard";
 import { useInfinitePosts } from "@/hook/queries/useFeedQueries";
 import { StringConstants } from "../common/CommonText";
+import PostFromFeed from "./PostFromFeed";
 
 export function Feed() {
   const userId = useSelector((state: RootState) => state.user.user?._id);
 
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    isLoading,
-  } = useInfinitePosts(userId);
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
+    useInfinitePosts(userId);
 
   const observer = useRef<IntersectionObserver | null>(null);
 
@@ -48,13 +44,15 @@ export function Feed() {
     );
   }
 
-  
   return (
     <div className="max-w-4xl mx-auto h-screen overflow-scroll scrollbar-hide">
       <Tabs defaultValue="feed" className="w-full">
+        <PostFromFeed />
         <TabsContent value="feed" className="mt-0 p-4">
           {posts.length === 0 ? (
-            <div className="text-center text-zinc-400">{StringConstants.NO_POST_AVAILABLE}</div>
+            <div className="text-center text-zinc-400">
+              {StringConstants.NO_POST_AVAILABLE}
+            </div>
           ) : (
             posts.map((post, index) => (
               <PostCarde
