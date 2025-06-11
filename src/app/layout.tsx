7a75abd992ae88c0dc0d77cff70bbc90fba033ca
Adarsh Singh <singhadarsh9540@gmail.com>
2025-06-11 23:18:52 +0530

    clarity is added
    Signed-off-by: Adarsh Singh <singhadarsh9540@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,19 @@ export default async function RootLayout({
             `,
           }}
         />
+         <ScriptAdd commentMore actions
+          id="ms-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "rgpxrvmq3a");
+            `
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
