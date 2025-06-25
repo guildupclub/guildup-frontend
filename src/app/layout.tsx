c7ea1ab2d72d71650d66317cf58017509ff0e01e
Adarsh Singh <singhadarsh9540@gmail.com>
2025-06-25 21:13:28 +0530

    feat: integrate Google Tag Manager script with optimized loading strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,15 +84,17 @@ export default async function RootLayout({
         <meta name="msapplication-TileColor" content="#0A0A0A" />
         <meta name="msapplication-config" content="/browserconfig.xml" />
         {/* Do not remove these script */} {/* Google Analytics */}
-        <script
+        <Script
+          id="gtm"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-           })(window,document,'script','dataLayer','GTM-5ZKZDMMK');
-         `,
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5ZKZDMMK');
+    `,
           }}
         />
         <script
