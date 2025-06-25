f9ea129371ca1d172a1ff6d28c62d9e076783ef5
Adarsh Singh <singhadarsh9540@gmail.com>
2025-06-25 22:59:42 +0530

    feat: add Facebook Pixel tracking integration for enhanced analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ import WelcomeBanner from "@/components/banner/Banner";
 import CookieConsent from "@/components/cookies/CookieConsent";
 import Script from "next/script";
 import RouteChangeTracker from "@/components/RouteChangeTracker";
-
+import dynamic from "next/dynamic";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -76,6 +76,23 @@ export default async function RootLayout({
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','GTM-5ZKZDMMK');
     `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?                         
+              n.callMethod.apply(n,arguments):n.queue.push   
+              (arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!
+              0;n.version='2.0';n.queue=[];t=b.createElement(e);
+              t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,
+              'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '661403980198126');
+              fbq('track', 'PageView');
+            `,
           }}
         />
         <meta
@@ -152,6 +169,15 @@ export default async function RootLayout({
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=661403980198126&ev=
+            PageView&noscript=1"
           />
         </noscript>
         <CookieConsentProvider>
