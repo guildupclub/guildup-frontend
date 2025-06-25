3603b62746cca04aae4d273280d8a5dcb46029a5
Adarsh Singh <singhadarsh9540@gmail.com>
2025-06-25 21:41:50 +0530

    feat: update Google Tag Manager script integration for improved loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,6 +65,19 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script
+          id="gtm"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5ZKZDMMK');
+    `,
+          }}
+        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover, interactive-widget=resizes-content"
@@ -84,19 +97,6 @@ export default async function RootLayout({
         <meta name="msapplication-TileColor" content="#0A0A0A" />
         <meta name="msapplication-config" content="/browserconfig.xml" />
         {/* Do not remove these script */} {/* Google Analytics */}
-        <Script
-          id="gtm"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-5ZKZDMMK');
-    `,
-          }}
-        />
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-B3B9W8GRQP"
