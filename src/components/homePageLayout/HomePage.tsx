0ec4e389478253502e68fea82ef614b77e2106b9
ayush.bhardwaj <ayyuuussh@gmail.com>
2025-07-29 02:22:10 +0530

    Feed page

--- src/components/homePageLayout/HomePage.tsx
+++ src/components/homePageLayout/HomePage.tsx
@@ -5,20 +5,22 @@ import { Feed } from "./Feed";
 
 export default function HomePage() {
   return (
-    <div className="h-screen flex flex-col"> {/* Add h-screen and flex-col */}
-      <div className="flex-grow flex"> {/* Add flex-grow to the main content */}
-        {/* Left sidebar hidden on small screens  */}
-        <div className="hidden md:block ps-20">
+    <div className="h-screen flex flex-col">
+      <div className="flex-grow flex">
+        {/* Left sidebar - fixed width with proper spacing */}
+        <div className="hidden lg:block w-80 flex-shrink-0 border-gray-200">
           <LeftSidebar />
         </div>
         
-        {/* Feed takes available space */}
-        <div className="flex-1">
-          <Feed />
+        {/* Feed - takes remaining space with proper margins */}
+        <div className="flex-1 min-w-0 px-2 lg:px-8">
+          <div className="max-w-3xl mx-auto">
+            <Feed />
+          </div>
         </div>  
         
-        {/* Right sidebar hidden on small screens */}
-        <div className="hidden md:block pe-20">
+        {/* Right sidebar - fixed width with proper spacing */}
+        <div className="hidden xl:block w-80 flex-shrink-0 border-gray-200">
           <RightSidebar />
         </div>   
       </div>
